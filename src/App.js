adbb600a7bb9bb5f9cf1b7050583e1cd9ad30d3d
avm71 <anmolmynam@gmail.com>
2022-04-26 22:03:18 -0400

    minor updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import './App.css';
-import { useState } from 'react';
-import MainMint from './MainMint';
-import NavigationBar from './NavigationBar';
+import './App.css'
+import { useState } from 'react'
+import MainMint from './MainMint'
+import NavigationBar from './NavigationBar'
 
 function App() {
-  const [accounts, setAccounts] = useState([]);
-  
-  return (<div className="App">
-    <NavigationBar accounts = {accounts} setAccounts= {setAccounts} />
-    <MainMint accounts = {accounts} setAccounts= {setAccounts} />
-  </div>);
+  const [accounts, setAccounts] = useState([])
+
+  return (
+    <div className="overlay">
+      <div className="App">
+        <NavigationBar accounts={accounts} setAccounts={setAccounts} />
+        <MainMint accounts={accounts} setAccounts={setAccounts} />
+      </div>
+      <div className="movingBackground"></div>
+    </div>
+  )
 }
 
-export default App;
+export default App
